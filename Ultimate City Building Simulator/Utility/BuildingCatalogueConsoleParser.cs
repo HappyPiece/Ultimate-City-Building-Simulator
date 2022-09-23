602ea74403eb5a7d2d83bd157dafa83e65c078ca
FnPngn AlexandroHIV <disturbedelk@gmail.com>
2022-09-23 22:39:53 +0700

    Building Catalogue Parser Formatting added

--- Ultimate City Building Simulator/Utility/BuildingCatalogueConsoleParser.cs
+++ Ultimate City Building Simulator/Utility/BuildingCatalogueConsoleParser.cs
@@ -11,13 +11,30 @@ namespace UltimateCityBuildingSimulator.Utility
     {
         public string Parse(BuildingCatalogue catalogue)
         {
-            string output = String.Empty;
+            //IReadOnlyList<BuildingCatalogue.Item> catalogueItems = catalogue.GetList();
+            //var output = new string[2, catalogueItems.Count];
+            //for (int index = 0; index < catalogueItems.Count; index++)
+            //{
+            //    output[0, index] = catalogueItems[index].Name.ToString();
+            //    output[1, index] = catalogueItems[index].Price.ToString();
+            //}
+            var output = $"{"Тип здания",-25} {"Стоимость",-25}\n";
+            //foreach (var item in catalogue.GetList())
+            //{
+            //    output = string.Join("", output, "building name: ", item.Name,
+            //        "\t", "current price: ", item.Price, "\n");
+            //}
             foreach (var item in catalogue.GetList())
             {
-                output = string.Join("", output, "building name: ", item.Name,
-                    "\t", "current price: ", item.Price, "\n");
+                output += $"{item.Name,-25} {item.Price,-25}\n";
             }
+            //return FormatOutputData(output);
             return output;
         }
+
+        //private string FormatOutputData(string data)
+        //{
+        //    return String.Format("{0,10}", data);
+        //}
     }
 }
